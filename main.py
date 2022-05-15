a3901ee5ab6d9e5c248bd576d9851f7eceb06faa
Joaquín <joaquincastanos@uc.cl>
2022-05-15 00:17:59 -0400

    Feat: fix lint issues

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
+"""Module to call main function"""
 from scrapper import Scrapper
-from pprint import pprint
+
 def main():
-
+    """Main function"""
     scrapper = Scrapper()
-    scrapper.runScrapper()
-    # print(f"----------------------------------------LETTER {scrapper.alphabet[scrapper.run_letter_counter]}----------------------------------------")
-    # for letra in scrapper.all_data:
-    #     scrapper.run_letter_counter += 1
-    #     if scrapper.run_letter_counter < len(scrapper.all_data):
-    #         if letra == []:
-    #             print("No available medications for letter: " + scrapper.alphabet[scrapper.run_letter_counter - 1])
-    #         print(letra)
-    #         print(f"\n----------------------------------------LETTER {scrapper.alphabet[scrapper.run_letter_counter]}----------------------------------------")
-    # pprint(scrapper.all_data[-1])
-    # print("----------------------------------------------------------")
+    scrapper.run_scrapper()
+    scrapper.write_data()
 
-    
-    # pprint(scrapper.all_data)
-    scrapper.writeData()
 if __name__ == "__main__":
     main()
